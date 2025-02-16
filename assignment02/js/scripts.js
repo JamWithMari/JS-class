61c354b0ecda5745cda5b0ccc01ff424f2907716
JamWithMari <jamaricharles@duck.com>
2025-02-16 11:38:55 -0500

    Feb 16 update

--- assignment02/js/scripts.js
+++ assignment02/js/scripts.js
@@ -9,8 +9,7 @@ const images =["../images/img1.jpg", "../images/img2.jpg", "../images/img3.jpg",
 
 // function to change bg color from user input and add student id
 function changeCustomColor(colorValue) {
-    
-    // console.log(`Color: ${colorValue}`);
+    //changing the background colour depending on the 
     if (colorValue < 0 || colorValue > 100) {
         document.body.style.backgroundColor = "red";
     }else if (colorValue > 0 && colorValue <= 20) {
@@ -24,17 +23,20 @@ function changeCustomColor(colorValue) {
     }else if(80 <colorValue && colorValue <= 100){
         document.body.style.backgroundColor = "yellow"; 
     }
-    studId.textContent = 10;
+    studId.textContent = "Student ID: 1219278"
 }
 
 function convertValue(){
-    let colorvalue = Number(colorNum.value);
+    //In the funciton we're taking the value of number inputed form the user and passing it inot the changeCustomColor function
+    let colorvalue = colorNum.value;
     changeCustomColor(colorvalue);
 }
 
 // function to change bg color from random no.
 function changeRandomColor() {
+    //this function generates a random number between -5 and 110 so we can get a red background more easier
     let randomColor = Math.floor(Math.random() * 111) - 5;
+    //We then take the generated number and pass it into the changeCustomColor function as the parameter
     changeCustomColor(randomColor);
 
 }
@@ -46,14 +48,16 @@ function addList() {
     
     // Tip: you might have to check length condition so that the list does not keep growing when clicked
     if(imgSelect.childElementCount <5 ){ 
-        //This if statement is checking 
+        //This if statement is checking to see if the other options haven't been generated yet then it deletes the first element
         if(imgSelect.childElementCount == 1){
             imgSelect.remove(0);
         }
+        //The for loop generates options element for every element in the images array
         for(let i =0; i <images.length; i++){
             const imgOption = document.createElement("option");
+            //I increased the option value by one just to make it more dynamic for the 
             imgOption.value = i+1;
-            imgOption.text =  `Image ${imgOption.value}`;
+            imgOption.textContent =  `Image ${imgOption.value}`;
             imgSelect.appendChild(imgOption);
            }
     }
@@ -63,11 +67,10 @@ function addList() {
 
 // function to change image
 function changeImage() {
-    console.log("the change image function has been called");
     
     //Subtracting 1 from the value to get the index of said image src from the images array
     let imgIndex = imgSelect.value -1;
-    console.log(imgIndex);
+    //Then we can take image index at set it ass the source of the image using the images array
     imgDisplayed.setAttribute("src", images[imgIndex]);
 
 }
